de554c5a1cc48b36183018215d18265ae0087c31
Amandeep <eng.aman29@gmail.com>
2025-03-23 21:28:40 +0530

    made setup.py file

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements() -> List[str]:
+    """
+    This function will return list of requirements
+    """
+    requirement_lst:List[str]=[]
+
+    try:
+        with open("requirements.txt",'r') as file:
+            lines=file.readlines()
+
+            for line in lines:
+                requirement=line.strip()
+                #ignore empty lines and -e.
+                if requirement and requirement != '-e .':
+                    requirement_lst.append(requirement)
+
+    except FileNotFoundError:
+        print("File not found!")
+
+    return requirement_lst
+
+setup(
+    name="Network Security ML project",
+    version="0.1",
+    author="Amandeep",
+    author_email="amantechwiz@example.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
